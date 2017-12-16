15fb11f67c631cf3d42839c2d3c1d4c1343b0bda
lutianlu <lutianlu888@163.com>
2017-12-16 10:09:33 +0800

    2017-12-16

--- filename_json.py
+++ filename_json.py
@@ -1,9 +1,14 @@
 import os
 
+for x in os.listdir(r'C:\Users\ThinkPads\Desktop\七期结业学员'):
+    print(x)
+
 z = []
 y = ['name', 'job', 'No', 'photo']
-with open(r'C:\Users\Administrator\Desktop\7.txt', 'r', encoding='UTF-8') as somefile:
+with open(r'C:\Users\ThinkPads\Desktop\7.txt', 'r', encoding='UTF-8') as somefile:
     for line in somefile:
-        z.append(dict(zip(y, line.split())))
-
+        n = line.split()
+        n.append('/_feidike/img/old/7/' + line.split()[0] + '.jpg')
+        z.append(dict(zip(y, n)))
+        # print(o)
 print(z)
