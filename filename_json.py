a92455b1a0544361d3b2c1f5bd1d24de5935e7cc
lutianlu <lutianlu888@163.com>
2020-04-12 21:11:24 +0800

    Update filename_json.py

--- filename_json.py
+++ filename_json.py
@@ -1,18 +1,73 @@
-import os
+# from functools import wraps
 
-for x in os.listdir(r'C:\Users\ThinkPads\Desktop\七期结业学员'):
-    print(x)
 
-z = []
-y = ['name', 'job', 'No', 'photo']
-with open(r'C:\Users\ThinkPads\Desktop\7.txt', 'r', encoding='UTF-8') as somefile:
-    for line in somefile:
-        n = line.split()
-        n.append('/_feidike/img/old/7/' + line.split()[0] + '.jpg')
-        z.append(dict(zip(y, n)))
-        # print(o)
-print(z)
+# def inject(func):
+#     @wraps(func)
+#     def wrap(self, **kwargs):
+#         self.__dict__ = kwargs
+#         return func(self, **kwargs)
+#     return wrap
 
-for x in os.listdir(r'C:\Users\ThinkPads\Desktop\七期结业学员'):
-    os.renames(r'C:\Users\ThinkPads\Desktop\七期结业学员\%s' % x,
-               r'C:\Users\ThinkPads\Desktop\七期结业学员\%s.jpg' % (x.split()[0]))
+
+# class Test:
+
+#     @inject
+#     def __init__(self, x, y):
+#         pass
+
+
+# t = Test(x=6, y=9)
+# print(t.__dict__)
+
+
+# class x(dict):
+
+#     def __init__(self,  **kwargs):
+#         super().__init__(self,  **kwargs)
+#         self.__dict__ = self
+#         print(self.__dict__, type(self))
+
+
+# y = x(x=7, y=8)
+# y['z'] = 9
+# print(y.x, y.y, y.__dict__)
+
+
+from time import time, sleep
+from contextlib import ContextDecorator
+
+
+class Timed:
+
+    def __enter__(self):
+        self.start = time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.end = time()
+        cost = self.end - self.start
+        print(f'Cost: {cost}')
+
+
+class Timed2(ContextDecorator):
+
+    def __enter__(self):
+        self.start = time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.end = time()
+        cost = self.end - self.start
+        print(f'Cost: {cost}')
+
+
+with Timed():
+    sleep(2)
+
+
+@Timed2()
+def f():
+    sleep(2)
+
+
+f()
